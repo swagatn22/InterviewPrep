67891d01cc91dbf3617fc496621fbf29c4770410
Mukul Madaan <mukul.madaan8@gmail.com>
2019-12-09 01:57:34 +0530

    Create FizzBuzz.cpp
    https://leetcode.com/problems/fizz-buzz/

---
+++ FizzBuzz.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector <string> s(n+1);
+        for(int i = 1; i < n+1; i++){
+            if(i%15 == 0){
+                s[i] = "FizzBuzz";
+            }
+            else if(i%3 == 0){
+                s[i] = "Fizz";
+            } else if(i%5 == 0){
+                s[i] = "Buzz";
+            }else {
+                s[i] = to_string(i);
+            }
+        }
+        s.erase(s.begin());
+        return s;
+    }
+};
